883c1a401236cb260009ff8535fb892132a72cb9
MacBook <macbook@M1-Waantech.local>
2024-12-09 11:28:38 +0500

    added : offers page with respionsiveness

---
+++ src/pages/scholarships/index.tsx
@@ -0,0 +1,9 @@
+import AppLayout from "@/components/Layouts/AppLayout";
+import React from "react";
+
+function Scholarship() {
+  return <div>Scholarship</div>;
+}
+
+Scholarship.Layout = AppLayout;
+export default Scholarship;
